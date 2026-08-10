4b1fbee0cdef3da7a47f61336a8836e7a5adc3e9
1PxShovon <shihabahmed5971@gmail.com>
2026-08-10 09:49:29 +0600

    Add files via upload

---
+++ Zayah Service List.dart
@@ -0,0 +1,20 @@
+                                 Zayah Offical services List
+
+Electrical Services 
+AC Servicing
+Plumbing Services
+Painting Services
+Welding Services
+Ceramic Tile Installation
+Gypsum Board Fittings
+
+Cleaning services
+                ❄️ AC Duct Cleaning
+                🪞 Floor Marble Polishing
+                🔥 Kitchen Hood Cleaning
+                🧹 Residential Cleaning
+                🪟 Window & Facade Cleaning              
+                🛠 Post-Construction Cleaning 
+                🛋 Upholstery & Carpet Cleaning
+                🏢 Commercial Cleaning
+                🧼 Deep Cleaning 
